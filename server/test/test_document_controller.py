b62a88f672d86e98463e3942822a2fc3fdcc49c5
thomasscothamilton <thomas.scot.hamilton@outlook.com>
2024-12-21 22:38:29 -0500

    Updating generated code

--- server/test/test_document_controller.py
+++ server/test/test_document_controller.py
@@ -1,95 +1,68 @@
 import unittest
-
+from unittest.mock import patch, MagicMock
 from flask import json
-
 from server.models.document import Document  # noqa: E501
 from server.test import BaseTestCase
+from server.controllers import document_controller
 
 
 class TestDocumentController(BaseTestCase):
     """DocumentController integration test stubs"""
 
-    def test_documents_get(self):
+    @patch('server.controllers.document_controller.Document')
+    def test_documents_get(self, mock_document):
         """Test case for documents_get
 
         Get all documents
         """
-        headers = { 
-            'Accept': 'application/json',
-        }
-        response = self.client.open(
-            '/documents',
-            method='GET',
-            headers=headers)
-        self.assert200(response,
-                       'Response body is : ' + response.data.decode('utf-8'))
+        result = document_controller.documents_get()
+        self.assertEqual(result, 'do some magic!')
 
-    def test_documents_id_delete(self):
+    @patch('server.controllers.document_controller.Document')
+    def test_documents_id_delete(self, mock_document):
         """Test case for documents_id_delete
 
         Delete a document by ID
         """
-        headers = { 
-        }
-        response = self.client.open(
-            '/documents/{id}'.format(id='id_example'),
-            method='DELETE',
-            headers=headers)
-        self.assert200(response,
-                       'Response body is : ' + response.data.decode('utf-8'))
+        result = document_controller.documents_id_delete('123')
+        self.assertEqual(result, 'do some magic!')
 
-    def test_documents_id_get(self):
+    @patch('server.controllers.document_controller.Document')
+    def test_documents_id_get(self, mock_document):
         """Test case for documents_id_get
 
         Get a document by ID
         """
-        headers = { 
-            'Accept': 'application/json',
-        }
-        response = self.client.open(
-            '/documents/{id}'.format(id='id_example'),
-            method='GET',
-            headers=headers)
-        self.assert200(response,
-                       'Response body is : ' + response.data.decode('utf-8'))
+        result = document_controller.documents_id_get('123')
+        self.assertEqual(result, 'do some magic!')
 
-    def test_documents_id_put(self):
+    @patch('server.controllers.document_controller.connexion')
+    @patch('server.controllers.document_controller.Document')
+    def test_documents_id_put(self, mock_document, mock_connexion):
         """Test case for documents_id_put
 
         Update a document by ID
         """
-        document = {"id":"id","title":"title","content":"content"}
-        headers = { 
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-        }
-        response = self.client.open(
-            '/documents/{id}'.format(id='id_example'),
-            method='PUT',
-            headers=headers,
-            data=json.dumps(document),
-            content_type='application/json')
-        self.assert200(response,
-                       'Response body is : ' + response.data.decode('utf-8'))
+        mock_connexion.request.is_json = True
+        mock_connexion.request.get_json.return_value = {'key': 'value'}
+        mock_document.from_dict.return_value = Document()
 
-    def test_documents_post(self):
+        result = document_controller.documents_id_put('123', {'key': 'value'})
+        self.assertEqual(result, {'document_id': '123', 'document': mock_document.from_dict.return_value})
+
+    @patch('server.controllers.document_controller.connexion')
+    @patch('server.controllers.document_controller.Document')
+    def test_documents_post(self, mock_document, mock_connexion):
         """Test case for documents_post
 
         Create a new document
         """
-        document = {"id":"id","title":"title","content":"content"}
-        headers = { 
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-        }
-        response = self.client.open(
-            '/documents',
-            method='POST',
-            headers=headers,
-            data=json.dumps(document),
-            content_type='application/json')
-        self.assert200(response,
-                       'Response body is : ' + response.data.decode('utf-8'))
+        mock_connexion.request.is_json = True
+        mock_connexion.request.get_json.return_value = {'key': 'value'}
+        mock_document.from_dict.return_value = Document()
+
+        result = document_controller.documents_post({'key': 'value'})
+        self.assertEqual(result, 'do some magic!')
 
 
 if __name__ == '__main__':
